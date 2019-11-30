c5be4acc8a6824753288ed48ff6a85b9058f4877
Krishna Kumar Parakkat <krishnakumar.parakkat@cognizant.com>
2019-11-29 19:46:23 -0600

    Added a ng bootstrap popup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule, NgbActiveModal } from '@ng-bootstrap/ng-bootstrap'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -11,6 +12,7 @@ import { ViewuserComponent } from './manageuser/viewuser/viewuser.component';
 import { ManageprojectComponent } from './manageproject/manageproject.component';
 import { AddprojectComponent } from './manageproject/addproject/addproject.component';
 import { ViewprojectComponent } from './manageproject/viewproject/viewproject.component';
+import { ModalComponent } from './modal/modal.component';
 
 @NgModule({
   declarations: [
@@ -20,16 +22,21 @@ import { ViewprojectComponent } from './manageproject/viewproject/viewproject.co
     ViewuserComponent,
     ManageprojectComponent,
     AddprojectComponent,
-    ViewprojectComponent
+    ViewprojectComponent,
+    ModalComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     FormsModule,
     ReactiveFormsModule,
-    HttpClientModule
+    HttpClientModule,
+    NgbModule.forRoot()
   ],
-  providers: [],
-  bootstrap: [AppComponent]
+  providers: [ NgbActiveModal ],
+  bootstrap: [AppComponent],
+  entryComponents: [
+    ModalComponent
+  ]
 })
 export class AppModule { }
--- src/app/manageproject/addproject/addproject.component.ts
+++ src/app/manageproject/addproject/addproject.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { Project } from 'src/app/Shared/project';
 import { ProjectService } from 'src/app/Shared/project.service';
 import { Router } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ModalComponent } from 'src/app/modal/modal.component';
 
 @Component({
   selector: 'app-addproject',
@@ -16,7 +18,7 @@ export class AddprojectComponent implements OnInit {
   actionName = "Add";
   todayDate: String;
   @Output() savedProject = new EventEmitter(); 
-  constructor(public projectservice:ProjectService, private router:Router) { }
+  constructor(public projectservice:ProjectService, private router:Router, private modalService: NgbModal) { }
 
   ngOnInit() {
     let today = new Date((new Date().getTime() - 24 * 60 * 60 * 1000));
@@ -24,19 +26,25 @@ export class AddprojectComponent implements OnInit {
   }
 
   onSubmit() {
-    
-    this.model.startdate = new Date(JSON.stringify(this.model.startdate));
-    this.model.enddate = new Date(JSON.stringify(this.model.enddate));
+    if (this.submitted)
+    {
+      this.model.startdate = new Date(JSON.stringify(this.model.startdate));
+      this.model.enddate = new Date(JSON.stringify(this.model.enddate));
 
-    if (this.actionName == "Add")
-      this.projectservice.addProject(this.model).subscribe(data => {this.saveStatus = data.response;this.savedProject.emit('Saved');});
-    else
-      this.projectservice.editProject(this.model).subscribe(data => {this.saveStatus = data.response; this.savedProject.emit('Saved');});
+      if (this.actionName == "Add")
+        this.projectservice.addProject(this.model).subscribe(data => {this.saveStatus = data.response;this.savedProject.emit('Saved');});
+      else
+        this.projectservice.editProject(this.model).subscribe(data => {this.saveStatus = data.response; this.savedProject.emit('Saved');});
 
+      this.submitted = false; 
+      this.model = new Project();
+      this.disableValidation = true;
+      this.actionName = "Add"
+    }
+  }
+  
+  onSubmitClick(): void{
     this.submitted = true; 
-    this.model = new Project();
-    this.disableValidation = true;
-    this.actionName = "Add"
   }
 
   resetProject(): void{
@@ -61,5 +69,11 @@ export class AddprojectComponent implements OnInit {
       //this.model.enddate = new Date(JSON.stringify(this.model.enddate));
     });
 
+  }
+
+  searchUser() {
+    // const modalRef = this.modalService.open(ModalComponent);
+    const modalRef = this.modalService.open(ModalComponent);
+    // modalRef.componentInstance.title = 'About';
   }
 }
